110c1470298368a8b29c127c594f36799a2c3931
zankoav <zankoav@mail.ru>
2019-02-03 12:09:57 +0300

    Little change

--- core/styles.php
+++ core/styles.php
@@ -12,22 +12,8 @@
 
 			wp_enqueue_style( 'commons', Assets::getCss( 'common' ), false, null );
 
-			if ( is_page_template( 'template-home.php' ) ) {
-				wp_enqueue_style( 'home', Assets::getCss( 'home' ), false, null );
-			} else if ( is_post_type_archive(['gonka']) ) {
-				wp_enqueue_style( 'gonki', Assets::getCss( 'gonki' ), false, null );
-			} else if ( is_page_template( 'template-faq.php' ) ) {
-				wp_enqueue_style( 'faq', Assets::getCss( 'faq' ), false, null );
-			} else if ( is_page_template( 'template-media.php' ) ) {
-				wp_enqueue_style( 'media-css', Assets::getCss( 'media' ), false, null );
-			} else if ( is_page_template( 'template-registration.php' ) ) {
-				wp_enqueue_style( 'registration-css', Assets::getCss( 'registration' ), false, null );
-			} else if ( is_singular( 'gonka' ) ) {
-				wp_enqueue_style( 'gonka', Assets::getCss( 'gonka' ), false, null );
-			} else if ( is_singular( 'post' ) ) {
-				wp_enqueue_style( 'single', Assets::getCss( 'single' ), false, null );
-			}else if ( is_archive() ) {
-				wp_enqueue_style( 'category', Assets::getCss( 'category' ), false, null );
+			if ( is_page_template( 'template-lesson-1.php' ) ) {
+				wp_enqueue_style( 'lesson_1', Assets::getCss( 'lesson_1' ), false, null );
 			}
 
 			wp_enqueue_style( 'style', BASE_URL . '/style.css', false, null );
